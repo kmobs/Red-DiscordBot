007d8fa0319e70762777ad470c3948be7f195485
Martin Brabham <optedoblivion@gmail.com>
2016-04-29 17:08:47 -0400

    Add chart command

--- cogs/yahoofinance.py
+++ cogs/yahoofinance.py
@@ -8,21 +8,29 @@ import urllib.parse
 class YahooFinance:
     """Image related commands."""
 
-    URL = "https://query.yahooapis.com/v1/public/yql?q=%s&env=http%%3A%%2F%%2Fdatatables.org%%2Falltables.env&format=json"
-    QUERY = """select * from yahoo.finance.quotes where symbol in ("%s")"""
+    QUOTE_URL = "https://query.yahooapis.com/v1/public/yql?q=%s&env=http%%3A%%2F%%2Fdatatables.org%%2Falltables.env&format=json"
+    QUOTE_QUERY = """select * from yahoo.finance.quotes where symbol in ("%s")"""
+
+    CHART_URL = "https://chart.finance.yahoo.com/t?s=%s&lang=en-US&region=US&width=300&height=180"
 
     def __init__(self, bot):
         self.bot = bot
         #Reserved for further ... stuff
 
-    def __build_url(self, symbols):
+    def __build_quote_url(self, symbols):
         """ 
-            Simple URL builder
+            Simple QUOTE_URL builder for building quote urls
         """
         derp = ",".join(symbols)
-        query = self.QUERY % derp
+        query = self.QUOTE_QUERY % derp
         query = urllib.parse.quote_plus(query)
-        return self.URL % query
+        return self.QUOTE_URL % query
+
+    def __build_chart_url(self, symbol):
+        """
+            Build urls for charts
+        """
+        return self.CHART_URL % symbol
 
     def __get_quote_list(self, *text):
         pass
@@ -36,7 +44,7 @@ class YahooFinance:
         """
         response = "%s [ %s | %s | %s ]"
         result = None
-        async with aiohttp.get(self.__build_url(text)) as r:
+        async with aiohttp.get(self.__build_quote_url(text)) as r:
             result = await r.json()
 
         quotes = []
@@ -68,7 +76,7 @@ class YahooFinance:
         """
         response = "Name: %s\nPercent Change: %s\nLast Trade Price: $%s\nAsk: $%s\nBid: $%s\nDay's low: $%s\nDay's high: $%s\nPrevious close: $%s\nOpen: $%s\n50-day SMA: $%s\n200-day SMA: $%s\nStock exchange: %s\nDividend yield: %s%%\nExpected dividend pay date: %s\n"
         result = None
-        async with aiohttp.get(self.__build_url(text)) as r:
+        async with aiohttp.get(self.__build_quote_url(text)) as r:
             result = await r.json()
 
         quotes = []
@@ -101,6 +109,10 @@ class YahooFinance:
             ddate = quote["ExDividendDate"]
             last_trade = quote["LastTradePriceOnly"]
             await self.bot.say(response % (name,pchange,last_trade,ask,bid,daylow,dayhigh,prevclose,open,fdsma,thdsma,exchange,dyield,ddate))
+
+    @commands.command(no_pm=True)
+    async def chart(self, *text):
+        await self.bot.say(self.__build_chart_url(text))
 
 def setup(bot):
     bot.add_cog(YahooFinance(bot))
